9ea0e230bb8b1295b79cca8132abccb1ed6895f8
annacano0 <anna.elisabet5@gmail.com>
2024-05-03 13:49:50 +0200

    add structure to component

---
+++ fakeSotre/src/components/eliminarProducto.vue
@@ -0,0 +1,20 @@
+<template>
+    <!-- Código VUE (JSX que luego transpila a JS) muy parecido a HTML -->
+  </template>
+
+  <script>
+    // Parte lógica de los datos --> JS
+    export default {
+      name: 'nombre-componente',
+    }
+  </script>
+ 
+  <style scoped>
+    /* CSS */
+  </style>
+
+
+
+
+
+
