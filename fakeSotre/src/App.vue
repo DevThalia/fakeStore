7f8f7d7642f7d976f29022731b98271997d7f080
Thalia Bravo <thalia.soler.7e7@itb.cat>
2024-05-03 13:44:27 +0200

    consumir api

--- fakeSotre/src/App.vue
+++ fakeSotre/src/App.vue
@@ -1,47 +1,32 @@
 <template>
   <div>
-    <p> {{product.title}} </p>
+    <p v-if="product">{{ product.title }}</p>
+    <p v-else>Cargando...</p>
   </div>
 </template>
 
 <script>
-
-/*import de los components*/
-export default{
+export default {
   name: 'App',
-  component: {
-    //componentes
-  },
-  props:{
-    //propiedades del proyecto
-  },
-  data(){
-    return{
-      product:null
+  data() {
+    return {
+      product: null
     };
   },
-  computed:{
-    //computadas
-  },
-  methods:{
-    async getProducto() {
-            try {
-                const res = await fetch(`https://fakestoreapi.com/products/${this.$route.params.id}`);
-                const data = await res.json();
-                if (!data || Object.keys(data).length === 0) {
-                    this.product=null;
-                    throw new Error("Producto no ecncontrado");
-                }
-                this.product = data;
-                this.error = '';
-            } catch (error) {
-                this.product = null;
-                this.error = "Producto no encontrado";
-            }
+  mounted() {
+    fetch('https://fakestoreapi.com/products/1')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('No se pudo obtener el producto');
         }
-  },
-  mounted(){
-    this.getProducto();
-  },
+        return response.json();
+      })
+      .then(data => {
+        this.product = data;
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
   }
+}
 </script>
