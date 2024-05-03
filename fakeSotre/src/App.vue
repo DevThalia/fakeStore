e2b345600dcecf4659fd19e24e82f2447de3af60
Thalia Bravo <thalia.soler.7e7@itb.cat>
2024-05-03 13:03:38 +0200

    estructura en app.vue

--- fakeSotre/src/App.vue
+++ fakeSotre/src/App.vue
@@ -1,47 +1,31 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Funciona montar app</h1>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+/*import de los components*/
+export default{
+  name: 'App',
+  component: {
+    //componentes
+  },
+  props:{
+    //propiedades del proyecto
+  },
+  data(){
+    return{
+      //att de la app
+    };
+  },
+  computed:{
+    //computadas
+  },
+  methods:{
+    //métodos de la app
+  },
+  mounted(){
+    //motarApp
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
   }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+</script>
