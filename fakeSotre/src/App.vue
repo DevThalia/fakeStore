734befdb4d0482397936bca63c1284c338b174f8
annacano0 <anna.elisabet5@gmail.com>
2024-05-03 13:26:53 +0200

    add 1 prod fetch from api

--- fakeSotre/src/App.vue
+++ fakeSotre/src/App.vue
@@ -1,5 +1,15 @@
 <template>
-  <h1>Funciona montar app</h1>
+ 
+  <header>
+     <h1></h1>
+     <nav>
+        <p>Categorias</p>
+        <p></p>
+     </nav>
+     <body>
+        <p>{{ product.id }}</p>
+     </body>
+  </header>
 </template>
 
 <script>
@@ -15,14 +25,28 @@ export default{
   },
   data(){
     return{
-      //att de la app
+      product:null
     };
   },
   computed:{
     //computadas
   },
   methods:{
-    //métodos de la app
+    async getProducto() {
+            try {
+                const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.pokemonName.trim().toLowerCase()}`);
+                const data = await res.json();
+                if (!data || Object.keys(data).length === 0) {
+                    this.product=null;
+                    throw new Error("Producto no ecncontrado");
+                }
+                this.product = data;
+                this.error = '';
+            } catch (error) {
+                this.product = null;
+                this.error = "Pokemon no encontrado";
+            }
+        }
   },
   mounted(){
     //motarApp
