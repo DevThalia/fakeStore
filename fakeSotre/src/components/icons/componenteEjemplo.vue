842cf6a605207835ba5da9a8a55f43d4af43654f
Thalia Bravo <thalia.soler.7e7@itb.cat>
2024-05-03 13:09:05 +0200

    componenteEjemplo

---
+++ fakeSotre/src/components/icons/componenteEjemplo.vue
@@ -0,0 +1,29 @@
+<template>
+<h1>Funciona montar componente ejemplo</h1>
+</template>
+
+<script>
+    export default {
+        name: 'componenteEjemplo',
+        component: {
+            //componentes
+        },
+        props:{
+            //propiedades del proyecto
+        },
+        data(){
+            return{
+                //att de la app
+            };
+        },
+        computed:{
+            //computadas
+        },
+        methods:{
+            //métodos de la app
+        },
+        mounted(){
+            //motarApp
+        }
+    }
+</script>
