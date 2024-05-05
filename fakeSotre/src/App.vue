1f858d29a1e8e86b768e8d6ac1750f16b0cfa0a8
Anna <anna.elisabet5@gmail.com>
2024-05-05 18:49:46 +0200

    create cartaProduct.vue component

--- fakeSotre/src/App.vue
+++ fakeSotre/src/App.vue
@@ -2,17 +2,27 @@
   <div>
     <p v-if="product">{{ product.title }}</p>
     <p v-else>Cargando...</p>
+    <cartaProduct class="carta" :product="product"></cartaProduct>
   </div>
 </template>
 
 <script>
+import cartaProduct from './components/cartaProduct.vue';
+
+
 export default {
   name: 'App',
+  components:{
+    cartaProduct
+  },
+  props:{'poduct-prop':Object},
+
   data() {
     return {
       product: null
     };
   },
+
   mounted() {
     fetch('https://fakestoreapi.com/products/1')
       .then(response => {
