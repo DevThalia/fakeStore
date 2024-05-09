0efa4e6f3054053f43ec5faca4908becdf0d7d33
ThaliaBravo <thaliasolerbravo2@gmail.com>
2024-05-09 23:00:13 +0200

    agregar, eliminar  y modificar producto

--- fakeSotre/src/components/eliminarProducto.vue
+++ fakeSotre/src/components/eliminarProducto.vue
@@ -1,20 +1,99 @@
 <template>
-    <!-- Código VUE (JSX que luego transpila a JS) muy parecido a HTML -->
-  </template>
+  <div>
+    <h2>Eliminar Producto</h2>
+    <div v-if="product" class="producto">
+      <h3>{{ product.title }}</h3>
+      <p><span>Precio:</span> {{ product.price }} $</p>
+      <p><span>Descripción:</span> {{ product.description }}</p>
+      <p><span>Categoría:</span> {{ product.category }}</p>
+      <p><span>Rating:</span> {{ product.rating.rate }}⭐ ({{ product.rating.count }})</p>
+      <img :src="product.image" :alt="product.title">
+      <button @click="eliminarProducto">Eliminar Producto</button>
+    </div>
+    <div v-else>
+      Cargando...
+    </div>
+  </div>
+</template>
 
-  <script>
-    // Parte lógica de los datos --> JS
-    export default {
-      name: 'nombre-componente',
+<script>
+export default {
+  name: 'eliminarProducto',
+  props: {
+    productId: Number
+  },
+  data() {
+    return {
+      product: null,
+    };
+  },
+  /*
+  mounted() {
+    fetch('https://fakestoreapi.com/products/' + this.productId.toString())
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('No se pudo obtener el producto');
+        }
+        return response.json();
+      })
+      .then(data => {
+        this.product = data;
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  },*/
+  methods: {
+  eliminarProducto() {
+    if (this.productId) {
+      // Emitir evento para eliminar el producto
+      this.$emit('eliminar-producto', this.productId);
+    } else {
+      alert("Por favor, introduce el ID del producto a eliminar.");
     }
-  </script>
- 
-  <style scoped>
-    /* CSS */
-  </style>
+  }
+}
 
+};
+</script>
 
+<style scoped>
+h2 {
+  width: 100%;
+  padding: 0em 0em 0.25em 1em;
+  border-bottom: 2px solid #333; 
+  margin-bottom: 1.25em;
+}
 
+.producto {
+  color: black;
+  padding: 2em;
+  background-color: white;
+}
 
+.producto img {
+  width: 80%;
+}
 
+span {
+  color: crimson;
+  font-weight: bolder;
+}
 
+button {
+  margin-top: 1em;
+  font-weight: bold;
+  font-size: 1em;
+  padding: 0.5em 1em;
+  border: none;
+  border-radius: 0.5em;
+  background-color: crimson;
+  color: white;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+button:hover {
+  background-color: rgb(148, 5, 34);
+}
+</style>
--- fakeSotre/src/components/modificarProducto.vue
+++ fakeSotre/src/components/modificarProducto.vue
@@ -1,20 +1,118 @@
 <template>
-    <!-- Código VUE (JSX que luego transpila a JS) muy parecido a HTML -->
-  </template>
+  <div>
+    <h2>Modificar Producto</h2>
+    <div v-if="product" class="producto">
+      <div>
+        <label for="title">Título:</label>
+        <input type="text" id="title" v-model="product.title" required>
+      </div>
+      <div>
+        <label for="price">Precio:</label>
+        <input type="number" id="price" v-model.number="product.price" required>
+      </div>
+      <div>
+        <label for="description">Descripción:</label>
+        <textarea id="description" v-model="product.description" required></textarea>
+      </div>
+      <div>
+        <label for="category">Categoría:</label>
+        <input type="text" id="category" v-model="product.category" required>
+      </div>
+      <div>
+        <label for="image">URL de la imagen:</label>
+        <input type="url" id="image" v-model="product.image" required>
+      </div>
+      <button @click="guardarCambios">Guardar Cambios</button>
+      <button @click="cancelar">Cancelar</button>
+    </div>
+    <div v-else>
+      Cargando...
+    </div>
+  </div>
+</template>
 
-  <script>
-    // Parte lógica de los datos --> JS
-    export default {
-      name: 'nombre-componente',
+<script>
+export default {
+  name: 'modificarProducto',
+  props: {
+    productId: Number
+  },
+  data() {
+    return {
+      product: null,
+    };
+  },
+  mounted() {
+    if (this.productId) {
+      fetch('https://fakestoreapi.com/products/' + this.productId.toString())
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('No se pudo obtener el producto');
+          }
+          return response.json();
+        })
+        .then(data => {
+          this.product = data;
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
     }
-  </script>
- 
-  <style scoped>
-    /* CSS */
-  </style>
+  },
+  methods: {
+    guardarCambios() {
+      if (this.product) {
+        this.$emit('guardar-cambios', this.product);
+      } else {
+        alert("No hay ningún producto para modificar.");
+      }
+    },
+    cancelar() {
+      this.$emit('cancelar-modificacion');
+    }
+  }
+};
+</script>
 
+<style scoped>
+h2 {
+  width: 100%;
+  padding: 0em 0em 0.25em 1em;
+  border-bottom: 2px solid #333; 
+  margin-bottom: 1.25em;
+}
 
+form div {
+  margin-bottom: 1em;
+}
 
+label {
+  display: block;
+  font-weight: bold;
+}
 
+input[type="text"],
+input[type="number"],
+input[type="url"],
+textarea {
+  width: 100%;
+  padding: 0.5em;
+}
 
+button {
+  margin-top: 1em;
+  font-weight: bold;
+  font-size: 1em;
+  padding: 0.5em 1em;
+  border: none;
+  border-radius: 0.5em;
+  background-color: crimson;
+  color: white;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
 
+button:hover {
+  background-color: rgb(148, 5, 34);
+}
+</style>
